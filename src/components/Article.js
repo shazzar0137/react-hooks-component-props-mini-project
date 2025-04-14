1606f72ce159e691769126e38350acfc149c9755
Daniel Kipngetich <danielkipngetich10@gmail.com>
2025-04-14 23:10:16 +0300

    updates /src/components folder

---
+++ src/components/Article.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Article({ title, date = "January 1, 1970", preview, minutes}){
+        const getEmoji = () => {
+          if (minutes < 30) {
+            const cups = Math.ceil(minutes / 5);
+            return "☕️".repeat(cups);
+          } else {
+            const bentos = Math.ceil(minutes / 10);
+            return "☕️".repeat(bentos);
+          }
+        };
+   
+    return(
+        <article>
+            <h3>{title}</h3>
+            <small>{date} • {getEmoji()} {minutes} min read</small>
+            <p>{preview}</p>
+        </article>
+    )
+}
+export default Article;
